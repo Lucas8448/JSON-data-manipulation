9083f11934c0dc744b31df6bfa1fdee3da03d47b
Lucas William Bateson <lucas.bateson@gmail.com>
2024-04-25 11:20:18 +0200

    Remove task requirements and add data for task 4

--- 3/main.py
+++ 3/main.py
@@ -1,12 +1,5 @@
 import json
 
-# task
-# Mål: Implementere søk og filtrering i en liste av JSON-objekter.
-# 
-#     Deloppgave 1: Last inn 3_Personer.json
-#     Deloppgave 2: Skriv en funksjon som filtrerer ut og viser personer basert på yrke.
-#     Deloppgave 3: Skriv en funksjon som finner den eldste personen i datasettet.
-
 with open("3_Personer.json", "r") as file:
     data = json.load(file)
 
